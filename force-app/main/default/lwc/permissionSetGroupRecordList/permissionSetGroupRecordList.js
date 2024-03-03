f8945238c5722f0ec6ba2c8f00c231f53884bb12
Zackary Frazier <zack11frazier22@gmail.com>
2024-03-03 00:21:25 -0800

    Update userRecordList and groupRecordList components

---
+++ force-app/main/default/lwc/permissionSetGroupRecordList/permissionSetGroupRecordList.js
@@ -0,0 +1,58 @@
+import { LightningElement } from "lwc";
+
+import DESCRIPTION_FIELD from "@salesforce/schema/PermissionSetGroup.Description";
+import LABEL_FIELD from "@salesforce/schema/PermissionSetGroup.MasterLabel";
+import LAST_MODIFIIED_DATE_FIELD from "@salesforce/schema/PermissionSetGroup.LastModifiedDate";
+import CREATED_DATE_FIELD from "@salesforce/schema/PermissionSetGroup.CreatedDate";
+
+const COLUMNS = [
+  {
+    label: "API Name",
+    fieldName: "Name",
+    type: "formattedLink"
+  },
+  {
+    label: "Label",
+    fieldName: LABEL_FIELD.fieldApiName,
+    type: "text"
+  },
+  {
+    label: "Description",
+    fieldName: DESCRIPTION_FIELD.fieldApiName,
+    type: "text"
+  },
+  {
+    label: "Last Modified Date",
+    fieldName: LAST_MODIFIIED_DATE_FIELD.fieldApiName,
+    type: "date"
+  },
+  {
+    label: "Created Date",
+    fieldName: CREATED_DATE_FIELD.fieldApiName,
+    type: "date"
+  },
+  {
+    label: "Status",
+    fieldName: "Status",
+    type: "text"
+  }
+];
+
+export default class PermissionSetGroupRecordList extends LightningElement {
+  columns = COLUMNS;
+  fields = [
+    "DeveloperName",
+    "MasterLabel",
+    "Description",
+    "LastModifiedDate",
+    "CreatedDate",
+    "Status"
+  ];
+
+  get newRecordLink() {
+    return `/flow/Create_Permission_Set_Group?retURL=${window.location}`;
+  }
+  get viewLink() {
+    return `/lightning/setup/PermSetGroups/page?address=/{{recordId}}`;
+  }
+}
